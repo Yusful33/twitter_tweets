3689bcd5e4c16ee64994ed5fcfcab30a82b297a9
yusful33 <ycattaneo33@gmail.com>
2022-08-02 10:13:22 -0500

    Getting JSON of replies

--- Pomp_tweets.py
+++ Pomp_tweets.py
@@ -1,6 +1,6 @@
 # import Twitter_Creds
 import datetime
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date 
 import os
 import pandas as pd
 import itertools
@@ -12,51 +12,75 @@ import re
 import networkx
 import warnings
 import json
-
 import yaml
 
+_NAME = "APompliano"
 with open("auth.yaml", "r") as creds:
     try:
         twitter_auth = yaml.safe_load(creds)
     except yaml.YAMLError as exc:
         print(exc)
 
-print(twitter_auth['consumer_key'])
 
-
-# # Authenticate to Twitter
+# # # Authenticate to Twitter
 auth = tw.OAuthHandler(twitter_auth['consumer_key'], twitter_auth['consumer_secret'])
 auth.set_access_token(twitter_auth['access_token'], twitter_auth['access_token_secret'])
 
-# # Create API object
-api = tw.API(auth, 
-wait_on_rate_limit=True)
-#,
-  #  wait_on_rate_limit_notify=True)
-
-#Getting Info On Twitter Users
-user = api.get_user("APompliano")
+# # # Create API object
+api = tw.API(auth, wait_on_rate_limit=True)
 
 
-search_term = "best article you read this week -filter:retweets"
+tweets = api.user_timeline(screen_name=_NAME, 
+                           # 200 is the maximum allowed count
+                           count=100,
+                           include_rts = False,
+                           # Necessary to keep full_text 
+                           # otherwise only the first 140 words are extracted
+                           tweet_mode = 'extended'
+                           )
+for info in tweets[:1]:
+  print(info)
 
-#Getting today's date
-current = date.today()
-d1 = current.strftime("%Y-%m-%d")
+# #Getting Info On Twitter Users
+# user = api.get_user(screen_name=_NAME)
 
-#Ten Days Ago
-days_ago = 10
-d2 = str(current - timedelta(days=days_ago))
+# search_term = "best article you read this week -filter:retweets"
+
+# #Getting today's date
+# current = date.today()
+# d1 = current.strftime("%Y-%m-%d")
+
+# #Ten Days Ago
+# days_ago = 10
+# d2 = str(current - timedelta(days=days_ago))
 
 
-tweets = tw.Cursor(api.search,
-                   q=search_term,
-                   lang="en",
-                   since=d2,
-                   until=d1
-).items(1000)
+# tweets = tw.Cursor(api.search_tweets,
+#                    q=search_term,
+#                    lang="en",
+#                    until=d1
+# ).items(100)
 
-all_tweets = [tweet.text for tweet in tweets]
+# replies = []
+# for tweet in tweets:
+#   if tweet.user.id_str == '339061487':
+#     print(tweet.id, tweet.user.screen_name, tweet.text, tweet.created_at, tweet.in_reply_to_status_id_str, tweet.in_reply_to_user_id, tweet.retweet_count, tweet.favorite_count)
 
-all_tweets[:5]
+#     for tweet in tw.Cursor(api.search_tweets,q='to:'+_NAME, since_id='339061487', tweet_mode='extended').items():
+#       if hasattr(tweet, 'in_reply_to_status_id_str'):
+#         replies.append(tweet)
+# print(replies[:1])
+
+
+
+# replies = []
+# for full_tweet in all_tweets:
+#   for tweet in tw.Cursor(api.search_tweets,q='to:'+_NAME, since_id=tweet_id, tweet_mode='extended').items():
+#     if hasattr(tweet, 'in_reply_to_status_id_str'):
+#       if (tweet.in_reply_to_status_id_str==full_tweet.id_str):
+#         replies.append(tweet.text)
+#   for elements in replies:
+#        print("Replies :",elements)
+#   replies.clear()
+# sample = all_tweets[0]
 
--- twitter_setup.py
+++ twitter_setup.py
@@ -1,14 +1,12 @@
 import yaml
 import logging
 import tweepy as tw
-from datetime import datetime, timedelta, date 
-import datetime
 
 _CONSUMER_SECRET = 'consumer_secret'
 _CONSUMER_KEY = 'consumer_key'
 _ACCESS_TOKEN_SECRET = 'access_token_secret'
 _ACCESS_TOKEN = 'access_token'
-_SEARCH_TERM = "best article you read this week -filter:retweets"
+_IMPORTANT_FIELDS = ['id', 'created_at', 'in_reply_to_status_id', 'text', 'retweet_count', 'favorite_count'] 
 
 def read_yaml(yaml_file):
     """Read Yaml file."""
@@ -34,14 +32,23 @@ class TwitterClient():
     def user_name(self):
         user = self.twitter_client.get_user(screen_name=self.twitter_user)
         print(type(user))
-        return user 
+        return user.screen_name
 
-    def search_tweets(self, _SEARCH_TERM):
-        current = date.today()
-        d1 = current.strftime("%Y-%m-%d")
-        tweets = tw.Cursor(self.twitter_client.search_tweets,
-                   q=_SEARCH_TERM,
-                   lang="en",
-                   until=d1
+    def search_tweets(self, search_term):
+        tweets = tw.Cursor(self.twitter_client.search_30_day,
+                    label='dev',
+                   query=search_term,
                     ).items(1)
         return tweets
+    
+    def get_tweet_responses(self, filter_param, tweet_id, from_date, to_date):
+        replies=[]
+        for tweet in tw.Cursor(self.twitter_client.search_30_day, 
+                                label='dev', 
+                                query=filter_param, 
+                                fromDate=from_date, toDate=to_date).items(50):
+            if hasattr(tweet, 'in_reply_to_status_id_str'):
+                if tweet.in_reply_to_status_id == tweet_id:
+                    field_dict = dict((k, tweet._json[k]) for k in _IMPORTANT_FIELDS if k in tweet._json)
+                    replies.append(field_dict)
+        return replies
